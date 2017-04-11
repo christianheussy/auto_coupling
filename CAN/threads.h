08b9b6e039874df3778a7e0773a3a936d319cca1
Christian Heussy <heussyc@seattleu.edu>
2017-04-11 15:55:57 +0000

    threads.h created online with Bitbucket

---
+++ CAN/threads.h
@@ -0,0 +1,10 @@
+#ifndef THREADS_H
+#define THREADS_H
+
+void Steering();        // Forward declaration for steering
+
+void Transmission();    // Forward declaration for speed control
+
+void Brakes();          // Forward declaration for Brakes
+
+#endif
