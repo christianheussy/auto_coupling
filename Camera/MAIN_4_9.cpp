b4af28481aab2eef7f456b0bfd84bef461150246
heussyc <heussy@gmail.com>
2017-04-16 13:33:38 -0700

    Small changes to truck driver

--- Camera/MAIN_4_9.cpp
+++ Camera/MAIN_4_9.cpp
@@ -85,11 +85,11 @@ int CheckStat(canStatus stat){
 void Steering()
 {
     int message_count{}, checksum_temp{}, checksum_calc{};
-    
+
     long steering_command_ID = 0x18FFEF27;
     unsigned int steering_command_DLC = 8;            //Data length
     unsigned int steering_command_FLAG = canMSG_EXT;  //Indicates extended ID
-    
+
     hnd1 = canOpenChannel(0,  canOPEN_REQUIRE_EXTENDED);        // Open channel for Steer thread
     stat=canSetBusParams(hnd1, canBITRATE_250K, 0, 0, 0, 0, 0); // Set bus parameters
     CheckStat(stat);
@@ -97,7 +97,7 @@ void Steering()
     CheckStat(stat);
     stat=canBusOn(hnd1);                                        //Take channel on bus
     CheckStat(stat);
-    
+
     while(true)
     {
         message_count++;
@@ -112,7 +112,7 @@ void Steering()
         steer_data[4] = ((steering_command & 0xFF000000) >> 24);
         steer_data[5] = 0xFF;
         steer_data[6] = 0xFF;
-        
+
         //Check Sum Calculation
         checksum_temp = steer_data[0] + steer_data[1] + steer_data[2] +
         steer_data[3] + steer_data[4] + steer_data[5] + steer_data[6] +
@@ -121,23 +121,23 @@ void Steering()
         ((steering_command_ID & 0x00FF0000) >> 16) +
         ((steering_command_ID & 0xFF000000) >> 24) +
         (message_count);
-        
+
         checksum_calc = ((checksum_temp >> 4) + checksum_temp) & 0x000F;
-        
+
         steer_data[7] =  (checksum_calc << 4) + (message_count); // put checksum into last byte
-        
+
         stat=canWriteWait(hnd1, steering_command_ID, steer_data, steering_command_DLC, steering_command_FLAG,50);
         //CheckStat(stat);
-        
+
         if (exit_flag == 1){
             break;
         }
-        
+
         this_thread::yield();
         this_thread::sleep_for (chrono::milliseconds(10));
-        
+
     }
-    
+
     stat = canBusOff(hnd1); // Take channel offline
     CheckStat(stat);
     canClose(hnd1);
@@ -145,7 +145,7 @@ void Steering()
 
 
 void Transmission() {// Thread used to control the speed using the transmission
-    
+
     long Drive_ID = 0x18FF552B;
     unsigned int Drive_DL = 8;
     unsigned int Drive_FLAG = canMSG_EXT;
@@ -158,7 +158,7 @@ void Transmission() {// Thread used to control the speed using the transmission
     CheckStat(stat);
     while (true)
     {
-        
+
         speed_data[0] = ((speed_command & 0x3F) << 2) + auto_park_enable;
         speed_data[1] = ((speed_command & 0x3FC0) >> 6);
         speed_data[2] = (braking_active << 4) + (direction << 2) + ((speed_command & 0xC000) >> 14);
@@ -169,14 +169,14 @@ void Transmission() {// Thread used to control the speed using the transmission
         speed_data[7] = 0xFF;
         stat=canWrite(hnd2, Drive_ID, speed_data, Drive_DL, Drive_FLAG);
         //CheckStat(stat);
-        
+
         if (exit_flag == 1){
             break;
         }
-        
+
         this_thread::yield();
         this_thread::sleep_for (chrono::milliseconds(10));
-        
+
     }
     stat = canBusOff(hnd2); // Take channel offline
     CheckStat(stat);
@@ -184,7 +184,7 @@ void Transmission() {// Thread used to control the speed using the transmission
 }
 
 void Brakes() {//Thread to Apply Brakes
-    
+
     hnd3 = canOpenChannel(1, canOPEN_REQUIRE_EXTENDED);         // Open channel for speed control
     stat=canSetBusParams(hnd3, canBITRATE_250K, 0, 0, 0, 0, 0); // Set bus parameters
     CheckStat(stat);
@@ -192,15 +192,15 @@ void Brakes() {//Thread to Apply Brakes
     CheckStat(stat);
     stat=canBusOn(hnd3);                                        // take channel on bus and start reading messages
     CheckStat(stat);
-    
+
     int brake_pressure_value = 15; // 8 bar
     int brake_pressure_command;
-    
+
     brake_pressure_command = (brake_pressure_value & 0x000000FF);
     long Brake_ID = 0x750;
     unsigned int Brake_DL = 8; //3 Bytes ??
     unsigned int Brake_FLAG = {}; //Indicates extended ID
-    
+
     while (true)
     {
         brake_data[0] = brake_pressure_command; //Front Left
@@ -216,15 +216,15 @@ void Brakes() {//Thread to Apply Brakes
         else if (braking_active == 0){
             brake_data[5] = 0;
         }
-        
+
         stat = canWrite(hnd3, Brake_ID, brake_data, Brake_DL, {});
         this_thread::yield();
         this_thread::sleep_for (chrono::milliseconds(10));
-        
+
         if (exit_flag == 1){
             break;
             }
-        
+
     }
     stat = canBusOff(hnd3); // Take channel offline
     CheckStat(stat);
@@ -235,16 +235,16 @@ int main(int argc, char** argv)
 {
     // Launch CAN THREADS
     canInitializeLibrary(); //Initialize driver
-    
+
 	//FOR TESTING ONLY
 	//std::string Coupling = "/media/ubuntu/SDCARD/Indoor_testing_3_20_4.svo";
 	ofstream mystream;
 	mystream.open("/home/ubuntu/Documents/SeniorProject/remotetrucks/Camera/Camera_TestData/DrivingTestData1.txt");
-	
-	
+
+
 	// Init time stamp 1
 	high_resolution_clock::time_point init_t1 = high_resolution_clock::now();
-	
+
 	// Initialize ZED color stream in HD and depth in QUALITY mode
 	sl::Camera zed; //add a filepath here to run code from recorded video
 	sl::InitParameters params;
@@ -262,44 +262,44 @@ int main(int argc, char** argv)
 		zed.close();
 		return 1;
 	}
-	
-	// Set the Depth Clamp to an appropriate value 
-	int depth_clamp = 20000;																	
-	zed.setDepthMaxRangeValue(depth_clamp);														
+
+	// Set the Depth Clamp to an appropriate value
+	int depth_clamp = 20000;
+	zed.setDepthMaxRangeValue(depth_clamp);
 
 	// Initialize image size (height and width)
 	// create empty openCV mats to contain ZED camera images
 	// ZED camera data will be pulled into a ZED mat data type then converted to openCV mat
-	int width = zed.getResolution().width;														
+	int width = zed.getResolution().width;
 	int height = zed.getResolution().height;
-	sl::Mat left(zed.getResolution(), sl::MAT_TYPE_8U_C4);													
-	cv::Mat left_image(height, width, CV_8UC4, left.getPtr<sl::uchar1>(sl::MEM_CPU));	
-	
+	sl::Mat left(zed.getResolution(), sl::MAT_TYPE_8U_C4);
+	cv::Mat left_image(height, width, CV_8UC4, left.getPtr<sl::uchar1>(sl::MEM_CPU));
+
 	// Init time stamp 2
-	high_resolution_clock::time_point init_t2 = high_resolution_clock::now();		
-	auto init_duration = duration_cast<milliseconds>( init_t2 - init_t1 ).count();		
-	std::cout << "Init duration: " << init_duration << "msec" << endl;					
+	high_resolution_clock::time_point init_t2 = high_resolution_clock::now();
+	auto init_duration = duration_cast<milliseconds>( init_t2 - init_t1 ).count();
+	std::cout << "Init duration: " << init_duration << "msec" << endl;
 
 	// crosshairs (xHair, yHair) are used for initial trailer selection and continuous tracking along the path
-	int xHair = left_image.cols / 2; 															
+	int xHair = left_image.cols / 2;
 	int yHair = left_image.rows / 2;
-	 															
+
 	// counter to limit the number of times edge and contour detection are performed
 	int count = 0;
-	
+
 	// auto park enable
 	bool start = true;
-	            
+
 	float limit = 0.0;
 	float a, b, x_cam, y_cam, x_fwheel, y_fwheel, dist_grad, y_cam_next, y_fwheel_next, st_coeff;
 
 	for (;;)
-	{	
-		sl::ERROR_CODE err = zed.grab(sl::SENSING_MODE::SENSING_MODE_STANDARD);			
+	{
+		sl::ERROR_CODE err = zed.grab(sl::SENSING_MODE::SENSING_MODE_STANDARD);
 		if (!err)
 		{
 			// create a ZED Mat to house the image from the left camera (left), then convert to openCV Mat
-			err = zed.retrieveImage(left, sl::VIEW_LEFT, sl::MEM_CPU);												
+			err = zed.retrieveImage(left, sl::VIEW_LEFT, sl::MEM_CPU);
 		} else if (err == sl::ERROR_CODE::ERROR_CODE_NOT_A_NEW_FRAME) {
 			cout << "Warning: got same frame from zed.grab as last time" << endl;
 			continue;
@@ -308,12 +308,12 @@ int main(int argc, char** argv)
 			cout << err << endl;
 			return 1;
 		}
-		
+
 		if (left_image.empty()) break; // end of video stream
-		
+
         if (adjustCrosshairsByInput(xHair, yHair, left_image.rows, left_image.cols)){
             cout << "Press Brake and Shift into Drive" << endl;
-         /* 
+         /*
         // While loop to check if brake pedal is pressed and driver has shifted into D, if so then autopark enable is set to 1
             while(brake_pedal != 1 && requested_gear != 68){
                 // Read brake pedal signal
@@ -326,15 +326,16 @@ int main(int argc, char** argv)
                 brake_pedal = ((0xC0 & brake_pedal_data[0]) >> 6);
             }
             * */
+
         auto_park_enable = 1;
         break;
         }
-		
-		
+
+
 		drawCrosshairsInMat(left_image, xHair, yHair);
 		imshow("this is you, smile! :)", left_image);
 		cvWaitKey(10);
-	
+
 }
 	std::thread t1(Steering); // Start thread for steering control
     t1.detach();
@@ -343,17 +344,17 @@ int main(int argc, char** argv)
     std::thread t3(Brakes);  // Start thread to read
 	t3.detach();
 
-	for (;;) 
+	for (;;)
 	{
-			
+
 		// For loop time stamp 1
 		high_resolution_clock::time_point for_t1 = high_resolution_clock::now();
-	
+
 		err = zed.grab(sl::SENSING_MODE::SENSING_MODE_STANDARD);
 		if (!err)
 		{
 			// create a ZED Mat to house the image from the left camera (left), then convert to openCV Mat
-			err = zed.retrieveImage(left, sl::VIEW_LEFT, sl::MEM_CPU);																	
+			err = zed.retrieveImage(left, sl::VIEW_LEFT, sl::MEM_CPU);
 		} else if (err == sl::ERROR_CODE::ERROR_CODE_NOT_A_NEW_FRAME) {
 			cout << "Warning: got same frame from zed.grab as last time" << endl;
 			continue;
@@ -366,18 +367,18 @@ int main(int argc, char** argv)
 
 		int thresh = 30;
 		int blur = 1;
-				
+
 		//count so this section doesn't happen every time
 		count++;
 		if (count == 1) {
 			count = 0;
-			
+
 			//edge detection left image
 			cv::Mat left_edges(height, width, CV_8UC4,1);
 			cvtColor(left_image, left_edges, COLOR_BGR2GRAY);
 			GaussianBlur(left_edges, left_edges, Size(7,7), blur, blur);
 			Canny(left_edges, left_edges, thresh, thresh*3, 3);
-			
+
 			//contour detection left image
 			cv::Mat l_contours = cv::Mat::zeros(left_edges.rows, left_edges.cols, CV_8UC4);
 			vector<vector<Point> > left_contours;
@@ -392,49 +393,49 @@ int main(int argc, char** argv)
 			namedWindow( "Left Contours", 1);
 			imshow( "Left Contours", l_contours);
 			cvWaitKey(10);
-			
+
 			// create a ZED Mat to house the depth map values
 			sl::Mat distancevalue;
 			err = zed.retrieveMeasure(distancevalue, sl::MEASURE::MEASURE_XYZ, sl::MEM_CPU);
-			
-			int leftedge;	// left edge of trailer 
-			int rightedge;	// right edge of trailer 
+
+			int leftedge;	// left edge of trailer
+			int rightedge;	// right edge of trailer
 			int x_center;	// horizontal center between left and right trailer edges
 			int y_center;	// vertical center between top and bottom trailer edges
 			float l1;		// distance in m to the left edge of the trailer
 			float l2;		// distance in m to the right edge of the trailer
-			
+
 			// adjusted coordinate values for the right and left trailer edges
 			// these values are shfted inward by 'pixel_shift'
-			int left_coord;  			
-			int right_coord;			
-			int pixel_shift = 3;		
-						
+			int left_coord;
+			int right_coord;
+			int pixel_shift = 3;
+
 			coordinateGrab(l_contours, xHair, yHair, leftedge, rightedge, x_center, y_center);
-			
+
 			left_coord = leftedge;
 			left_coord += pixel_shift;
 			right_coord = rightedge;
 			right_coord -= pixel_shift;
-			
+
 			distanceGrab(l1, l2, left_coord, right_coord, y_center, distancevalue);
-			
+
 			// this ensures that the distance is grabbed from the front face of the trailer and not the sides
 			if(min(l1, l2) < 10)
 				pixel_shift = 4;
 			else if(min(l1,l2) < 7)
-				pixel_shift = 5;	
-			
+				pixel_shift = 5;
+
 			float center_dist;
 			float theta_1;
 			float theta_2;
 			float range = 1024.0;
-          
+
 			pathInputCalculations_Camera(l1, l2, center_dist, theta_1, theta_2, left_coord, right_coord);
             st_coeff = range*RMIN/2;
-            
 
-            
+
+
             cout << "d = " << center_dist << endl;
 			cout << "t1 = " << theta_1 << endl;
 			cout << "t2 =" << theta_2 << endl;
@@ -446,7 +447,7 @@ int main(int argc, char** argv)
 				y_cam = center_dist*sinf(theta_1);
 				x_fwheel = x_cam - L*cosf(theta_2);
 				y_fwheel = y_cam - L*sinf(theta_2);
-		
+
 
 				dist_grad = x_fwheel / RES;				// Set distance gradient
 				y_cam_next = a*pow(x_cam - dist_grad, 2) + b*pow(x_cam - dist_grad, 3);
@@ -458,11 +459,11 @@ int main(int argc, char** argv)
 				cout << "                                Impossible path" << endl;
 				braking_active = 1;
 			}
-			
+
 			cout << x_cam << endl;
 			cout << y_cam << endl;
-			
-			            
+
+
             // FOR TESTING ONLY
 			mystream  << l1 << ","
 					 << l2 << ","
@@ -471,26 +472,26 @@ int main(int argc, char** argv)
 					 << theta_2 << ","
  					 << steering_command << std::endl;
 
-                
+
 			if (start)
 			{
 			// Prompt user
-          
-            speed_command = 500; // Set speed to .75kph and begin to drive straight back
-            
+
+            speed_command = 500; // Set speed to .5kph and begin to drive straight back
+
             start = false;
 			}
-            
+
             //cout << "we got this far" << endl;
 
-			
+
 			xHair = x_center;
 			yHair = y_center;
 			if(max(l1, l2) < 20)
-				depth_clamp = 1000*max(l1, l2) + 2000;	
+				depth_clamp = 1000*max(l1, l2) + 2000;
 			std::cout << "max range= " << zed.getDepthMaxRangeValue() << std::endl;
 			std::cout << "depth_clamp= " << depth_clamp << std::endl;
-			zed.setDepthMaxRangeValue(depth_clamp);		
+			zed.setDepthMaxRangeValue(depth_clamp);
 		}
 
 		drawCrosshairsInMat(left_image, xHair, yHair);
@@ -498,23 +499,23 @@ int main(int argc, char** argv)
 		// "Why cvWaitKey?"
 		// http://stackoverflow.com/questions/5217519/what-does-opencvs-cvwaitkey-function-do
 		cvWaitKey(10);
-		
+
 		// For loop time stamp 2
 		high_resolution_clock::time_point for_t2 = high_resolution_clock::now();
-		auto forloop_duration = duration_cast<milliseconds>( for_t2 - for_t1 ).count();	
+		auto forloop_duration = duration_cast<milliseconds>( for_t2 - for_t1 ).count();
 		//std::cout << "For loop duration: " << forloop_duration << "msec" << endl;
 
 	}
 	// FOR TESING ONLY
 	mystream.close();
 	zed.close();
-	
+
         /*
         t1.join(); // Wait for t1 to finish
         t2.join(); // Wait for t2 to finish
         t3.join(); // Wait for t3 to join
         */
-        
+
 	return 0;
 }
 
